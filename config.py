4063520e5e78e18db0409b9ffce69e33b2a8d509
itLinkee <emmanuel.p@linkee.co>
2025-07-23 16:12:26 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+SEARCH = {
+    "keywords": ["studio Paris", "meublé"],
+    "department": 75,
+    "radius_km": 5,
+    "price_min": 400,
+    "price_max": 800,
+    "max_age_hours": 24
+}
+CREDENTIALS = {
+    "login": "votre_email",
+    "password": "votre_mdp"
+}
+TEMPLATE = "templates/message.html"  # Jinja2
+CALLBACK_SLOTS = ["2025-07-28 10:00", "2025-07-28 14:00"]
